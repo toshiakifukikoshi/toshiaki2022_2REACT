320dabbe4dfb4f13bcecd0a9a0c39c7b8cdb4f23
toshiakifukikoshi <86909359+toshiakifukikoshi@users.noreply.github.com>
2022-02-05 17:40:05 +0900

    aaa

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/ColorfulMessage";
 import Keiji from "./components/Keiji";
 
-import React from "react";
+// import React from "react";
+// import React from "react";
 
 export const App = () => {
   return (
