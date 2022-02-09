07f949809a79de4c4a65d555e4c6a5d00836b42e
toshiakifukikoshi <86909359+toshiakifukikoshi@users.noreply.github.com>
2022-02-09 13:15:28 +0900

    自分で作ってるReact

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { App } from "./App";
+
+const App = () => {
+  return <></>;
+};
 
 ReactDOM.render(<App />, document.getElementById("root"));
