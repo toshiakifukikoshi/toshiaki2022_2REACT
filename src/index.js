5ee16316a34eb5f097446910fbe49051fa964ddf
toshiakifukikoshi <86909359+toshiakifukikoshi@users.noreply.github.com>
2022-03-23 21:13:02 +0900

    3_20復習します

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import { App } from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+import ReactDom from "react-dom";
+
+const App = () => {
+  return null;
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
